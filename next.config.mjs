8bd9bdff8a31955ea827d77abf6ce6bfe3368f0d
Manvendra <manvendrakumar0023@gmail.com>
2026-04-23 11:56:21 +0530

    Preparing for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  reactStrictMode:false,
+
+  // Allow Ngrok URLs in development
+  allowedDevOrigins: [
+    "*.ngrok-free.dev",
+  ],
+
+  images:{
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "randomuser.me"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
